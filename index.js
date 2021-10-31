46c67fabdcba2732d74882fa1302948c82971953
Rakib32000 <rh938664@gmail.com>
2021-10-31 07:53:42 -0700

    my server commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhrl2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("hitting the database")
+        const database = client.db("toursiom");
+        const servicesCollection = database.collection("services");
+        const orderCollection = database.collection('orders');
+
+        // get api
+        app.get('/addServices', async (req, res) => {
+            const cursor = servicesCollection.find({})
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+
+
+
+
+        // POST API
+        app.post('/addServices', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+        });
+
+
+        // get api
+        app.get('/getOrders', async (req, res) => {
+            const cursor = orderCollection.find({})
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+
+
+        // Add Orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        })
+
+
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log("my server is working", port)
+})
